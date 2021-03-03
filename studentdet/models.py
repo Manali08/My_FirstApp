32bcc4844ed3d98aa01b6a8b5223a3bea76ee1e1
Manali08 <manalijangam99@gmail.com>
2021-03-03 21:03:09 +0530

    Initial commit

---
+++ studentdet/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    sid = models.CharField(max_length=20)
+    sname = models.CharField(max_length=100)
+    semail = models.EmailField()
+    scontact = models.CharField(max_length=30)
